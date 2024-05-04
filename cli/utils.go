ec4e8b03ea2fea791361f80a50d49f112039ef4e
danielbarajas-rescale <danielbarajas@rescale.com>
2024-05-04 08:16:42 -0700

    adding transfer method

--- cli/utils.go
+++ cli/utils.go
@@ -1,9 +1,13 @@
 package cli
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
 	"errors"
 	"flag"
 	"fmt"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 	"log"
 	"os"
 	"regexp"
@@ -12,7 +16,7 @@ import (
 
 func ParseBucket(bucketPath string) (string, string, error) {
 	if !strings.HasPrefix(bucketPath, "gs://") {
-		return "", "", errors.New("Invalid bucket. Bucket must start with gs://")
+		return "", "", errors.New("invalid bucket. Bucket must start with gs://")
 	}
 
 	re := regexp.MustCompile("^gs://(.+?)/(.+)")
@@ -33,6 +37,7 @@ func ParseArgs(args []string) (string, string) {
 	if *help || len(cmd.Args()) != 2 {
 		Usage()
 	}
+
 	src := cmd.Arg(0)
 	dest := cmd.Arg(1)
 	return src, dest
@@ -50,4 +55,27 @@ func Usage() {
 	fmt.Printf(usage)
 
 	os.Exit(1)
+}
+
+func GetGoogleClient(ctx context.Context) (*storage.Client, error) {
+	credentialsString, err := getGoogleCredentials()
+	if err != nil {
+		return nil, err
+	}
+
+	credentials, err := google.CredentialsFromJSON(ctx, []byte(credentialsString), "https://www.googleapis.com/auth/cloud-platform")
+	if err != nil {
+		return nil, err
+	}
+
+	return storage.NewClient(ctx, option.WithCredentials(credentials))
+}
+
+func getGoogleCredentials() (string, error) {
+	credentials := os.Getenv("GCP_APPLICATION_CREDENTIALS")
+	if credentials == "" {
+		return "", errors.New("error GCP_APPLICATION_CREDENTIALS not set")
+	}
+
+	return credentials, nil
 }
